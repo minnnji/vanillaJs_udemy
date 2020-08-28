8ee4c8d7f68ae3a96c65fcf253dcc2c45e3857a6
minnnji <choimj2326@gmail.com>
2020-08-28 22:32:33 +0900

    [02]Feat: select seats, update count & price, set localStorage items

---
+++ seat-booking/script.js
@@ -0,0 +1,40 @@
+const container = document.querySelector('.container');
+const seats = document.querySelectorAll('.row .seat:not(.occupied)');
+const count = document.getElementById('count');
+const total = document.getElementById('total');
+const movieSelect = document.getElementById('movie');
+
+let ticketPrice = movieSelect.value;
+
+function setMovieData(movieIndex, moviePrice) {
+  localStorage.setItem('selectedMovieIndex', movieIndex);
+  localStorage.setItem('selectedMoviePrice', moviePrice);
+}
+
+function updateSelectedCount() {
+  const selectedSeats = document.querySelectorAll('.row .seat.selected');
+
+  const seatsIndex = [...selectedSeats].map(seat => [...seats].indexOf(seat));
+  localStorage.setItem('selectedSeats', JSON.stringify((seatsIndex)));
+
+  const selectedSeatsCount = selectedSeats.length;
+  count.innerText = selectedSeatsCount.toString();
+  total.innerText = (selectedSeatsCount * ticketPrice).toString();
+}
+
+movieSelect.addEventListener('change', e => {
+  ticketPrice = +e.target.value;
+  setMovieData(e.target.selectedIndex, e.target.value);
+  updateSelectedCount();
+});
+
+container.addEventListener('click', e => {
+  if (
+    e.target.classList.contains('seat') &&
+    !e.target.classList.contains('occupied')
+  ) {
+    e.target.classList.toggle('selected');
+
+    updateSelectedCount();
+  }
+});
