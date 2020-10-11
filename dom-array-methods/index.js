883d6f275797c18d095e7b426ed6090398b5e879
Minji-Choi <minjichoi@dable.io>
2020-10-11 21:52:28 +0900

    [05]Feat: map, sort, filter, reduce

--- dom-array-methods/index.js
+++ dom-array-methods/index.js
@@ -24,6 +24,34 @@ async function getRandomUser() {
   addData(newUser);
 }
 
+function doubleMoney() {
+  data = data.map(user => {
+    return { ...user, money: user.money * 2 }
+  });
+
+  updateDOM();
+}
+
+function sortByRichest() {
+  data.sort((a, b) => b.money - a.money);
+
+  updateDOM();
+}
+
+function showMillionaires() {
+  data = data.filter(user => user.money > 1000000);
+
+  updateDOM();
+}
+
+function calculateWealth() {
+  const wealth = data.reduce((acc, user) => (acc += user.money), 0);
+
+  const wealthEl = document.createElement('div');
+  wealthEl.innerHTML = `<h3>Total Wealth: <strong>${formatMoney(wealth)}</strong></h3>`;
+  main.appendChild(wealthEl);
+}
+
 function addData(obj) {
   data.push(obj);
 
@@ -46,3 +74,7 @@ function formatMoney(number) {
 }
 
 addUserBtn.addEventListener('click', getRandomUser);
+doubleBtn.addEventListener('click', doubleMoney);
+sortBtn.addEventListener('click', sortByRichest);
+showMillionairesBtn.addEventListener('click', showMillionaires);
+calculateWealthBtn.addEventListener('click', calculateWealth);
